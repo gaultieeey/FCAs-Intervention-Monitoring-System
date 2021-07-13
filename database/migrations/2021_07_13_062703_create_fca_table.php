06a9bc645b982d19b303b289b8238bda49dc50e6
Gaultier Cua <gaultiercua@gmail.com>
2021-07-13 15:34:38 +0800

    create fca migration

---
+++ database/migrations/2021_07_13_062703_create_fca_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFcaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fca', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('fca_name');
+            $table->date('application_received_date');
+            $table->date('date_accredited');
+            $table->string('fca_type');
+            $table->string('barangay');
+            $table->string('municipality');
+            $table->string('province');
+            $table->string('contact_person');
+            $table->integer('contact_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fca');
+    }
+}
